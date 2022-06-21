586737e09bf57ac9f84502f0b21c642449569d4a
Sunil C M <you@example.com>
2022-06-21 19:43:23 +0530

    commiting MethodOverloading

---
+++ Addition1.java
@@ -0,0 +1,19 @@
+class Addition1{
+public static void main(String a[]){
+	
+	
+int additionWithTwonumbers =add(56,56);
+System.out.println(additionWithTwonumbers);
+
+add(15,18,17);
+}
+ static int add(int a,int b)
+{
+	return a+b;
+}
+ static void add(int a,int b,int c){
+System.out.println (a+b+c);
+}
+}
+
+
---
+++ Division.java
@@ -0,0 +1,13 @@
+class Division{
+public static void main(String a[]){
+int finalValue =div(49,7);
+System.out.println(finalValue);
+
+int finalValue1=div(81,9);
+System.out.println(finalValue1);
+}
+public static int div(int a,int b)
+{
+	return a/b;
+}
+}
